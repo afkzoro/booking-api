251a31cff15cfa308ba947501ee5a83ba474fe6e
Samuel Theophilus <samueltheophilus019@gmail.com>
2025-08-09 03:04:56 +0100

    Refactor app module to include TypeOrm configuration and update module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BookingModule } from './booking/booking.module';
-import { PropertyModule } from './property/property.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { PropertiesModule } from './properties/properties.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BookingModule, PropertyModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT || 5432),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // dev only - use migrations in prod
+    }),
+    BookingsModule,
+    PropertiesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
